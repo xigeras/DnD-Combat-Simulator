7349677f07f46cfbc5b85410c8466be834c8a0bd
august b. <69170271+xigeras@users.noreply.github.com>
2025-06-30 08:19:54 +0000

    getters, setters, toString, and isDefeated methods

---
+++ Combatant.java
@@ -0,0 +1,48 @@
+public class Combatant {
+    public String name;
+    public int hp;
+    public int initiative;
+    public String type;
+
+    public Combatant(String name, int hp, int initiative, String type) {
+        this.name = name;
+        this.hp = hp;
+        this.initiative = initiative;
+        this.type = type;
+    }
+
+    // getter methods
+    public String getName() {
+        return name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public int getInitiative() {
+        return initiative;
+    }
+    public String getType() {
+        return type;
+    }
+    // setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String toString() {
+        return """ + name + " - HP: " + hp + ", Initiative: " + initiative + ", Type: " + type + """;
+    }
+    
+    public boolean isDefeated() {
+        return hp <= 0;
+    }
+}
